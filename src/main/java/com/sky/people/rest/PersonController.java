839a69b405fe1a2821ff20c9cb5c1975ddc73a0a
Jamie Evans <115733254+WilliamJamieEvans@users.noreply.github.com>
2023-02-08 12:14:54 +0000

    added getAll

--- src/main/java/com/sky/people/rest/PersonController.java
+++ src/main/java/com/sky/people/rest/PersonController.java
@@ -1,16 +1,34 @@
 package com.sky.people.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.sky.people.domain.Person;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class PersonController {
+
+    private List<Person> people = new ArrayList<>();
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String greeting() {
         return "Hello, World!";
     }
+
+    @PostMapping("/create")
+    public Person addPerson(@RequestBody Person person) {
+        this.people.add(person); // ad a new person to the list
+        return this.people.get(this.people.size() - 1); // return the last person in the list
+    }
+
+
+
+    @GetMapping("/getAll")
+    public List<Person> getAll() {
+        return this.people;
+    }
+
 
 
 }
