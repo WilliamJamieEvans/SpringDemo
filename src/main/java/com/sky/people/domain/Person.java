9269343abb6607e8ca1c8d0b56e0a667b0e1309e
Jamie Evans <115733254+WilliamJamieEvans@users.noreply.github.com>
2023-02-08 15:01:05 +0000

    validation complete and included

--- src/main/java/com/sky/people/domain/Person.java
+++ src/main/java/com/sky/people/domain/Person.java
@@ -1,11 +1,17 @@
 package com.sky.people.domain;
 
+import javax.validation.constraints.*;
+
 public class Person {
 
+    @Size(min = 2, max = 50)
     private String name;
 
+    @Min(0)
+    @Max(100) // annotations always go on top
     private int age;
 
+    @NotNull
     private String job;
 
 
