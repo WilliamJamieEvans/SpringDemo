dc16255277a476e8616aeda17849163f9fc4206d
Jamie Evans <115733254+WilliamJamieEvans@users.noreply.github.com>
2023-02-08 14:18:32 +0000

    added a update id

--- src/main/java/com/sky/people/rest/PersonController.java
+++ src/main/java/com/sky/people/rest/PersonController.java
@@ -3,6 +3,7 @@ package com.sky.people.rest;
 import com.sky.people.domain.Person;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,6 +38,18 @@ public class PersonController {
     @DeleteMapping("/remove/{id}")
     public Person removePerson (@PathVariable int id) {
         return this.people.remove(id);
+    }
+
+    @PatchMapping("/update/{id}")
+    public Person updatePerson(@PathVariable int id, @PathParam("name") String name, @PathParam("age")int age, @PathParam("job")String job) {
+            Person old = this.people.get(id);
+
+            if (name != null) old.setName(name);
+            if (age > 0) old.setAge(age);
+            if  (job != null) old.setJob(job);
+
+            return old;
+
     }
 
 
