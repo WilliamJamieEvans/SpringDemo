9c1a79b8da27782feac07022084161975e565729
Jamie Evans <115733254+WilliamJamieEvans@users.noreply.github.com>
2023-02-10 16:36:36 +0000

    added all tests

--- src/test/java/com/sky/people/rest/PersonIntegrationTest.java
+++ src/test/java/com/sky/people/rest/PersonIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.sky.people.rest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sky.people.domain.Person;
 import com.sky.people.personDTO.PersonDTO;
 import com.sky.people.personDTO.PersonReqDTO;
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,9 @@ import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // loads the app context with all your beans
 @AutoConfigureMockMvc // sets up the testing library
@@ -87,6 +91,21 @@ public class PersonIntegrationTest {
         PersonDTO person = new PersonDTO("NameTEST", 24,"SQLTEST");
         //CHECK THE RESPONSE BODY
         ResultMatcher checkBody = MockMvcResultMatchers.content().json(this.mapper.writeValueAsString(person));
+        // DO THE REQ AND CHECK THE RESPONSE
+        this.mvc.perform(req).andExpect(checkStatus).andExpect(checkBody);
+    }
+
+    @Test
+    void testGetAll() throws Exception{
+        //SETTING UP THE REQUEST
+        RequestBuilder req = MockMvcRequestBuilders.get("/getAll");
+        //CHECK THE RESPONSE STATUS
+        ResultMatcher checkStatus = MockMvcResultMatchers.status().isOk();
+        PersonDTO person = new PersonDTO("NameTEST", 24,"SQLTEST");
+        List<PersonDTO> personList = new ArrayList<>();
+        personList.add(person);
+        //CHECK THE RESPONSE BODY
+        ResultMatcher checkBody = MockMvcResultMatchers.content().json(this.mapper.writeValueAsString(personList));
         // DO THE REQ AND CHECK THE RESPONSE
         this.mvc.perform(req).andExpect(checkStatus).andExpect(checkBody);
     }
